aaa2ebac33b7327952aeac8a34d7ddc7439d8fd1
Josh Baron <joshbaron48@outlook.com>
2024-07-08 21:49:30 -0400

    remove pygame and work on execv

--- main.py
+++ main.py
@@ -10,35 +10,14 @@ from run_command import run_cmd
 def main():
     ps1: str = f"{os.getlogin()}@{socket.gethostname()}:{os.getcwd()} $"
     path: str = os.getenv("PATH")
-    inp: str = f"{ps1} "
-    currentLine = 0
-    oldStdout = sys.stdout
-
-    pygame.init()
-    size = width, height = 1280, 720
+    
     running = True
 
-    screen = pygame.display.set_mode(size)
-    font = pygame.freetype.Font("./RobotoMono.ttf", 16)
-
     while running:
-        screen.fill((0, 0, 0))
-        font.render_to(screen, (0,currentLine), inp, (255, 255, 255))
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_BACKSPACE:
-                    if len(inp) > 0 and len(inp) != len(ps1)+1:
-                        inp = inp[:-1]
-                elif event.key != pygame.K_RETURN or event.key == pygame.K_TAB:
-                    inp += event.unicode
-                elif event.key == pygame.K_RETURN:
-                    run_cmd(inp[:(len(ps1)+1)])
-                    if oldStdout != sys.stdout:
-                        currentLine += 1
-                        font.render_to(screen, (0,currentLine), sys.stdout, (255, 255, 255))
-        pygame.display.flip()
+        ps1: str = f"{os.getlogin()}@{socket.gethostname()}:{os.getcwd()} $"
+        stuff = input(ps1 + " ")
+        if stuff:
+            run_cmd(stuff)
 
 
 if __name__ == "__main__":
--- run_command.py
+++ run_command.py
@@ -1,12 +1,43 @@
 import os
+import signal
 
 
 def run_cmd(command):
-    cmd = command.split()[0]
+    command = command.split()
+    cmd = ""
     flags = []
-    
+    args = []
+    for i in range(0, len(command)):
+        if i == 0:
+            cmd = command[i]
+        elif command[i][0] == '-':
+            flags.append(command[i])
+        else:
+            args.append(command[i])
+
+    """
+    this match statement checks for shell builtins and if it is not a shell builtin, forks and runs the exe with execv
+    if it exists in /usr/bin
+    """
     match cmd:
         case "cd":
-            os.chdir(cmd[1])
+            if not args:
+                os.chdir(f'/home/{os.getlogin()}')
+            else:
+                try:
+                    os.chdir(str(args[0]))
+                except FileNotFoundError:
+                    print("No such file or directory: " + args[0])
         case _:
-            sys.
+            processid = os.fork()
+            signal.signal(signal.SIGINT, signal.default_int_handler)
+            if processid > 0:
+                os.waitpid(0, 0)
+            else:
+                try:
+                    try:
+                        os.execv(f"/usr/bin/{cmd}", ([f"/usr/bin/{cmd}"] + flags + args))
+                    except FileNotFoundError:
+                        print("No such file or directory: " + cmd)
+                except KeyboardInterrupt:
+                    return
